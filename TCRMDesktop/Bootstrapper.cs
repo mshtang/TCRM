52d4608b55ee320c122eccd28bbee3214ef82552
mshtang <32868278+mshtang@users.noreply.github.com>
2020-11-11 22:38:57 +0100

    Setup MVVM project with Caliburn.Micro

---
+++ TCRMDesktop/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using TCRMDesktopUI.ViewModels;
+
+namespace TCRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<BaseViewModel>();
+        }
+    }
+}
