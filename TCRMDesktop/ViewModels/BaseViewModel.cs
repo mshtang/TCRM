11d9f8e5372b432ca8285f1f37fd510b59567609
mshtang <32868278+mshtang@users.noreply.github.com>
2020-11-22 13:08:17 +0100

    Replace container instance in BaseViewModel

--- TCRMDesktop/ViewModels/BaseViewModel.cs
+++ TCRMDesktop/ViewModels/BaseViewModel.cs
@@ -6,19 +6,16 @@ namespace TCRMDesktopUI.ViewModels
     public class BaseViewModel : Conductor<object>, IHandle<LogInEvent>
     {
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
         private IEventAggregator _events;
 
-        public BaseViewModel(IEventAggregator events, SimpleContainer container, SalesViewModel salesVM)
+        public BaseViewModel(IEventAggregator events, SalesViewModel salesVM)
         {
             _events = events;
             _events.Subscribe(this);
 
-            _container = container;
-
             _salesVM = salesVM;
 
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogInEvent logInEvent)
